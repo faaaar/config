2cf9d52ad6fdf438a4732a03f5f1e1b0755554f7
LIYUNFAN <leeyunfans@gmail.com>
2021-04-08 16:54:18 +0800

    Update dotfiles
    Signed-off-by: LIYUNFAN <leeyunfans@gmail.com>

---
+++ .emacs.d/lisp/init-rust.el
@@ -0,0 +1,27 @@
+;;; init-rust.el --- Emasc Configuration
+
+;; Author: Yunfan Li <leeyunfans@gmail.com>
+;; URL: https://github.com/faaaar/dotfiles/tree/master/.emacs.d
+
+;;; Commentary:
+;;
+;; Rust configuration.
+;;
+
+;;; Code:
+
+(use-package rustic
+  :init
+  (setq lsp-rust-server 'rls)
+  (setq rustic-rustfmt-bin "~/.cargo/bin")
+  (setq rust-format-on-save t)
+  (setq rust-format-show-buffer nil)
+  :hook
+  (rust-mode . lsp-deferred)
+  :config
+  (use-package cargo))
+
+
+(provide 'init-rust)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; init-rust.el ends here
